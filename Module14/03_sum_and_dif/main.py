35fd8cd1c47874ae9dcae428c8c17d9da892893a
ten_v <vladislav.ten1987@gmail.com>
2021-11-04 18:20:53 +0500

    Версия операционной системы

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+
+def Nsumm(a):
+    a =str(a)
+    DigitSumm = 0
+    for i in a:
+        i = int(i)
+        DigitSumm += i
+    print('Сумму всех цифр: ', DigitSumm)
+    return(DigitSumm)
+
+def summDigits(a):
+    a = str(a)
+    count = 0
+    for i in a:
+        count += 1
+    print('Количество цифр в числе: ', count)
+    return(count)
+
+N = int(input('Введите число: '))
+a = Nsumm(N)
+b = summDigits(N)
+print('Разность суммы и кол-ва цифр:50', a - b)
