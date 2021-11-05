5ff2cfb9b73ffb4bb32c4cdd2efa28fb6495f58d
ten_v <vladislav.ten1987@gmail.com>
2021-11-05 13:12:19 +0500

    Версия операционной системы

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,30 @@
 # TODO здесь писать код
 
+def reverse(a):
+    flg = True
+    firstNum = ''
+    secondNum = ''
+    for i in a:
+        if i == '.':
+            flg = False
+        elif flg :
+            firstNum = i + firstNum
+        else:
+            secondNum = i + secondNum
+
+    e = firstNum + '.' + secondNum
+    return(e)
+
+
+N = input('Введите первое число: ')
+K = input('Введите второе число: ')
+a = reverse(N)
+b = reverse(K)
+print('\nПервое число наоборот: ', a)
+print('Первое число наоборот: ', b)
+print('Сумма ', round(float(a) + float(b), 2))
+
+
+
+
 
