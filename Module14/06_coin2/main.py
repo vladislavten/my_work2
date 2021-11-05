7473e08f791b720879c6e810ab4fef5dae428554
ten_v <vladislav.ten1987@gmail.com>
2021-11-05 13:56:19 +0500

    Монетка

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+
+def detect(x, y, r):
+  if x ** 2 + y ** 2 <= r ** 2:
+    print('Монетка где-то рядом')
+  else:
+    print('Монетки в области нет')
+
+x = float(input('Введите координату Х: '))
+y = float(input('Введите координату Y: '))
+r = float(input('Введите радиус: '))
+detect(x, y, r)
