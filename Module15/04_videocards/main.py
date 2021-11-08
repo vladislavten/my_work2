9d7c6a5109ef3d5e931f782c09063ddd5234f76a
kidalo <vladislav.ten1987@gmail.com>
2021-11-08 20:36:41 +0300

    Видеокарты

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,22 @@
 # TODO здесь писать код
 
+number = int(input('Введите кол-во видеокарт: '))
+count = 1
+models = []
+result = []
+_ = True
+for i in range(number):
+    print(count, 'Видеокарта:', end = ' ')
+    model = int(input())
+    models.append(model)
+    count += 1
 
+print('Старый список видеокарт: ', models)
+
+for x in models:
+    if x == max(models):
+        _ = False
+    else:
+        result.append(x)
+
+print('Новый список видеокарт:', result)
