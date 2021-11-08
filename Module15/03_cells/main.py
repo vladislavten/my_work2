5b300dc4d7545a6dc78617f39b08edbd7a78083b
kidalo <vladislav.ten1987@gmail.com>
2021-11-08 20:09:39 +0300

    Клетки

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+
+cell = int(input('Количество клеток: '))
+result = []
+
+for i in range(cell):
+    print('Эффективность', i + 1, 'клетки:', end = ' ')
+    rang = int(input())
+    if rang < i:
+        result.append(rang)
+print(result)
