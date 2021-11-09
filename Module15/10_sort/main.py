5b2ab3ef0156eb5aca8b0ad885fccf2082e384a6
ten_v <vladislav.ten1987@gmail.com>
2021-11-09 15:00:36 +0500

    Сортировка

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,10 +1,11 @@
 # TODO здесь писать код
 
 nums = [1, 4, -3, 0, 10]
+print('Изначальный список:', nums)
 
 for i in range(len(nums)):
     for x in range(i, len(nums)):
         if nums[x] < nums[i]:
             nums[x], nums[i] = nums[i], nums[x]
 
-print(nums)
+print('\nОтсортированный список:',nums)
