1198f6e754e457bb009a71ba270eacfcafd48ca6
ten_v <vladislav.ten1987@gmail.com>
2021-11-09 12:52:08 +0500

    Бегущие цифры

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+string = [1, 2, 3, 4, 5]
+move = int(input('Сдвиг: '))
+result = []
+print('Изначальный список:', string)
+
+for i in range(len(string)):
+    print(i, end = ' ')
+    result.append(string[i - move])
+
+print(result)
