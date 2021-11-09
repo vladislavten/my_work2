0dde4b8ca9f9f018bba994ec5cc9e1aa45455e02
ten_v <vladislav.ten1987@gmail.com>
2021-11-09 10:35:43 +0500

    Анализ слова

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+
+word = input('Введите слово: ')
+word = list(word)
+result = 0
+
+for i in range(len(word) ):
+    count = 0
+    index = 0
+    for x in word:
+        if x == word[i]:
+            count +=1
+    if count == 1:
+        result += 1
+
+print('Кол-во уникальных букв:',result)
