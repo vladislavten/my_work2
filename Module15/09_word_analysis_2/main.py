ac3d8757cb664f93727a5616ff2cb3a2796ed45b
ten_v <vladislav.ten1987@gmail.com>
2021-11-09 13:19:33 +0500

    Анализ слова 2

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+
+word = input('Введите слово: ')
+result = ''
+
+for i in word:
+    result = i + result
+
+if word == result:
+    print('Слово является палиндромом')
+else:
+    print('Слово не является палиндромом')
+
+
+
+
