076d6b45dfb23200c9205aac9f058776e736ca48
kidalo <vladislav.ten1987@gmail.com>
2021-11-10 18:18:36 +0300

    дз

---
+++ myWork/16 DZ.py
@@ -0,0 +1,32 @@
+# Задача 2. Шеренга
+# first_line = list(range(160, 176 + 1, 2))
+# print('Первая шеренга: ', first_line)
+# second_line = list(range(162, 180 + 1, 3))
+# print('Вторая шеренга: ', second_line)
+#
+# first_line.extend(second_line)
+#
+# for i in range(len(first_line)):
+#     for x in range(i, len(first_line)):
+#         if first_line[x] < first_line[i]:
+#             first_line[x], first_line[i] = first_line[i], first_line[x]
+#
+# print('\nОтсортированный список:',first_line)
+
+
+# Задача 3. Детали
+# shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300], ['педаль', 100], ['седло', 1500],
+#         ['рама', 12000], ['обод', 2000], ['шатун', 200], ['седло', 2700]]
+# enter = input('Название детали: ')
+# count_detail = 0
+# summ = 0
+# for i in range(len(shop)):
+#         if shop[i][0] == enter:
+#                 count_detail += 1
+#                 summ += shop[i][1]
+# print('\nКол-во деталей:', count_detail)
+# print('Общая стоимость:', summ)
+
+
+# Задача 4. Вечеринка
+guests = [‘Петя’, ‘Ваня’, ‘Саша’, ‘Лиза’, ‘Катя’]
