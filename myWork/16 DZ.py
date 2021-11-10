5679c0fa40861b9b85013cc749dfccaf8fde68ef
kidalo <vladislav.ten1987@gmail.com>
2021-11-10 18:59:37 +0300

    дз

--- myWork/16 DZ.py
+++ myWork/16 DZ.py
@@ -29,4 +29,20 @@
 
 
 # Задача 4. Вечеринка
-guests = [‘Петя’, ‘Ваня’, ‘Саша’, ‘Лиза’, ‘Катя’]
+guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+while True:
+    print('\nСейчас на вечеринке', len(guests),  'человек:', guests)
+    choice = input('Гость пришёл или ушёл? ')
+    if choice == 'Пора спать' or choice == 'пора спать':
+        print('Вечеринка закончилась, все легли спать.')
+        break
+    name = input('Имя гостя? ')
+    if choice == 'пришёл' or choice == 'пришел':
+        if len(guests) >= 6:
+            print('Прости,', name, ', но мест нет.')
+        else:
+            print('Привет,', name)
+            guests.append(name)
+    elif choice == 'ушёл' or choice == 'ушел':
+        print('Пока,', name)
+        guests.remove(name)
