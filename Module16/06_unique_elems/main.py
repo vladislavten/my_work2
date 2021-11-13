6a8d294de0bbe85b1f9c9e8925a4b62cb72fb365
kidalo <vladislav.ten1987@gmail.com>
2021-11-13 14:14:32 +0300

    Уникальные элементы

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+
+first_list = []
+second_list = []
+
+print('Первый 3 числа')
+for i in range(3):
+    print('Введите', i + 1, 'число:', end = ' ')
+    first = input()
+    first_list.append(first)
+
+print('\nВторые 7 чисел')
+for i in range(7):
+    print('Введите', i + 1, 'число:', end=' ')
+    first = input()
+    second_list.append(first)
+
+first_list.extend(second_list)
+first_list = list(set(first_list))
+
+print('Новый первый список с уникальными элементами:', first_list)
