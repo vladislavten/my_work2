545cf19895b07ca958c55b41f930d8af6ce076d0
kidalo <vladislav.ten1987@gmail.com>
2021-11-13 13:57:54 +0300

    Шеренга

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -3,3 +3,13 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
 # TODO здесь писать код
+
+enter = input('Название детали: ')
+count_detail = 0
+summ = 0
+for i in range(len(shop)):
+        if shop[i][0] == enter:
+                count_detail += 1
+                summ += shop[i][1]
+print('\nКол-во деталей:', count_detail)
+print('Общая стоимость:', summ)
