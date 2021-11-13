f915cdb8381bdc8d739a134ce811de1b8bd0767b
kidalo <vladislav.ten1987@gmail.com>
2021-11-13 14:19:22 +0300

    Друзья

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+friends = int(input('Кол-во друзей: '))
+friends_list = []
+dolg = int(input('Долговых расписок: '))
+
+for _ in range(friends):
+    friends_list.append(0)
+
+for i in range(dolg):
+    print(i + 1, 'расписка')
+    whom = int(input('Кому: '))
+    From = int(input('От кого: '))
+    hm = int(input('Сколько: '))
+
+    friends_list[whom - 1] -= hm
+    friends_list[From - 1] += hm
+
+print('\nБаланс друзей')
+for num in range(friends ):
+    print(num + 1, ':', friends_list[num])
