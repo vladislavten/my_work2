8764f6f00ee889338407f03191061becfc0a741a
kidalo <vladislav.ten1987@gmail.com>
2021-11-13 14:09:58 +0300

    Песни

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -11,3 +11,14 @@ violator_songs = [
 ]
 
 # TODO здесь писать код
+
+hm_songs = int(input('Сколько песен выбрать? '))
+time = 0
+for i in range(hm_songs):
+    print('Название', i + 1, 'песни:', end = ' ')
+    song_name = input()
+    for i_song in range(len(violator_songs)):
+        if song_name == violator_songs[i_song][0]:
+            time += violator_songs[i_song][1]
+
+print('\nОбщее время звучания песен:', round(time, 2))
