d64e15b3633e3b70eedac9834bce4460e05bff1a
kidalo <vladislav.ten1987@gmail.com>
2021-11-13 13:55:45 +0300

    Страшный код

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,37 @@
+# a = [1, 5, 3]
+# b = [1, 5, 1, 5]
+# c = [1, 3, 1, 5, 3, 3]
+# for i in b:
+#     a.append(i)
+# t = 0
+# for i in a:
+#     if i == 5:
+#         t += 1
+# print(t)
+# d = []
+# for i in a:
+#     if i != 5:
+#         d.append(i)
+# for i in c:
+#     d.append(i)
+# t = 0
+# for i in d:
+#     if i == 3:
+#         t += 1
+# print(t)
+# print(d)
+
+# TODO переписать программу
+
 a = [1, 5, 3]
 b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
 
-# TODO переписать программу
+a.extend(b)
+print('Кол-во цифр 5 при первом объединении:', a.count(5))
+for i in range(a.count(5)):
+    a.remove(5)
+a.extend(c)
+
+print('\nКол-во цифр 3 при втором объединении:', a.count(3))
+print('\nИтоговый список:', a)
