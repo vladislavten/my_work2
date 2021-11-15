052117b210a911ce6c81b59db516620ea520091e
ten_v <vladislav.ten1987@gmail.com>
2021-11-15 14:33:01 +0500

    Задача 2. Долги

---
+++ myWork/18.py
@@ -0,0 +1,24 @@
+# МОДУЛЬ 18.2 Форматирование строк: format и f-strings
+#
+# name = input('Введите имя: ')
+# order = input('Введите номер заказа: ')
+#
+# print('Здравствуйте, {name}! Ваш номер заказа: {order}. Приятного дня!'.format(
+#             name = name,
+#             order = order
+# ))
+
+
+# Задача 2. Долги
+
+# name = input('Введите имя: ')
+# dolg = input('Введите долг: ')
+#
+# print('{name}! {name}, привет! Как дела, {name}? Где мои {dolg} рублей? {name}! '.format(
+#             name = name,
+#             dolg = dolg
+# ))
+
+
+
+
