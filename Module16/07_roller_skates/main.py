fbf2edefa43821bd9e7c4e06e374c6d58b9249a3
ten_v <vladislav.ten1987@gmail.com>
2021-11-15 17:48:21 +0500

    Задача 7. Ролики

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,6 +1,13 @@
 # TODO здесь писать код
-skates_list = []
+def sort(nums):
+    for i in range(len(nums)):
+        for x in range(i, len(nums)):
+            if nums[x] < nums[i]:
+                nums[x], nums[i] = nums[i], nums[x]
+
 people_list = []
+skates_list = []
+
 skates = int(input('Кол-во коньков: '))
 for i in range(skates):
     print('Размер', i + 1, 'пары:', end = ' ')
@@ -12,6 +19,9 @@ for i in range(people):
     print('Размер ноги', i + 1, 'человека:', end = ' ')
     size = input()
     people_list.append(size)
+
+sort(people_list)
+sort(skates_list)
 
 count = 0
 for i in people_list:
