388f12bd8fdf0f84c99a7fa92f3f7017d24f70de
ten_v <vladislav.ten1987@gmail.com>
2021-11-15 17:46:23 +0500

    Задача 7. Ролики

--- myWork/16 DZ.py
+++ myWork/16 DZ.py
@@ -115,29 +115,39 @@
 
 
 # Задача 7. Ролики
-# skates_list = []
-# people_list = []
-# skates = int(input('Кол-во коньков: '))
-# for i in range(skates):
-#     print('Размер', i + 1, 'пары:', end = ' ')
-#     size = input()
-#     skates_list.append(size)
-#
-# people = int(input('\nКол-во людей: '))
-# for i in range(people):
-#     print('Размер ноги', i + 1, 'человека:', end = ' ')
-#     size = input()
-#     people_list.append(size)
-#
-# count = 0
-# for i in people_list:
-#     for x in skates_list:
-#         if i <= x:
-#             count += 1
-#             skates_list.remove(x)
-#             break
-#
-# print('Наибольшее кол-во людей, которые могут взять ролики: ', count )
+def sort(nums):
+    for i in range(len(nums)):
+        for x in range(i, len(nums)):
+            if nums[x] < nums[i]:
+                nums[x], nums[i] = nums[i], nums[x]
+
+people_list = []
+skates_list = []
+
+skates = int(input('Кол-во коньков: '))
+for i in range(skates):
+    print('Размер', i + 1, 'пары:', end = ' ')
+    size = input()
+    skates_list.append(size)
+
+people = int(input('\nКол-во людей: '))
+for i in range(people):
+    print('Размер ноги', i + 1, 'человека:', end = ' ')
+    size = input()
+    people_list.append(size)
+
+sort(people_list)
+sort(skates_list)
+
+count = 0
+for i in people_list:
+    for x in skates_list:
+        if i <= x:
+            count += 1
+            skates_list.remove(x)
+            break
+
+print('Наибольшее кол-во людей, которые могут взять ролики: ', count )
 
 
 # Задача 8. Считалка
