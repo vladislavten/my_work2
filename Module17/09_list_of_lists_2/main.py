34105536a4df510e0d1b879f4f5d60febc20f352
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:33:40 +0300

    Список списков 2

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -2,3 +2,7 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
 # TODO здесь писать код
+
+result = [nice_list[i][x][y] for i in range(len(nice_list)) for x in range(3) for y in range(3)]
+
+print('Ответ:', result)
