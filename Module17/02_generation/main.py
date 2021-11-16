aa46b50d117300a9f588df37fc2d51c78e3875bb
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:25:52 +0300

    Генерация

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,8 @@
 # TODO здесь писать код
+import random
+N = int(input('Введите число: '))
+
+lst_N = [random.randint(1, 100) for _ in range(N)]
+lst_N = [1 if i % 2 == 0 else i % 5 for i in range(0, len(lst_N))]
+
+print('Результат:', lst_N)
