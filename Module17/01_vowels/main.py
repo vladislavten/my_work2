1bd882a25066ce8e355076691935ecf6d48fa5a4
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:24:52 +0300

    Гласные буквы

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,8 @@
 # TODO здесь писать код
+vowels = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я']
+
+text = input('Введите текст: ')
+result = [letter for letter in text for x in vowels if letter == x]
+
+print('Список гласных букв: ', result)
+print('Длина списка:', len(result))
