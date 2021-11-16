f46ce266d95a10d2939248e3adf4e3f6df88df7a
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:31:20 +0300

    Развлечение

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+sticks = int(input('Кол-во палок: '))
+throws = int(input('Кол-во бросков: '))
+
+lst = ['|' for i in range(sticks)]
+
+for i in range(1, throws + 1):
+    print('Бросок', i, '. ', end = '')
+    print('Сбиты палки с номера', end = '')
+    a = int(input(' '))
+    print('по номер', end = '')
+    b = int(input(' '))
+    for z in range (a - 1, b):
+        lst[z] = '.'
+
+for i in lst:
+    print(i, end = '')
