2ac42042405387b55bd5e9f79df5a923c84a402f
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:30:15 +0300

    Двумерный список

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+
+import random
+def generate():
+    lst = [random.randint(1, 15) for _ in range(3)]
+    return lst
+
+result = [generate() for _ in range(4)]
+
+print('Результат:', result)
