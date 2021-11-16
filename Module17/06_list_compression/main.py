2a05ac98798b33609d663e9939bacb47d10c878f
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:29:35 +0300

    Сжатие списка

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,7 @@
 # TODO здесь писать код
+N = [2, 3, 0, 4, 0, 4, 0, 3, 56, 0, 2, 0, 34, 0, 3]
+
+result = [i for i in N if i > 0]
+zeros = [i for i in N if i == 0]
+result.extend(zeros)
+print(result)
