eb5f83bde5cb37cfacacaebf0a7236c6c9472876
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:27:47 +0300

    Тренируемся со срезами

--- Module17/04_slices/main.py
+++ Module17/04_slices/main.py
@@ -1,3 +1,15 @@
 alphabet = 'abcdefg'
 
 # TODO здесь писать код
+
+copy_alphahet = alphabet[:]
+print(copy_alphahet)
+print(alphabet[::-1])
+print(alphabet[::2])
+print(alphabet[1::2])
+print(alphabet[:1])
+print(alphabet[6:5:-1])
+print(alphabet[3:4])
+print(alphabet[4:])
+print(alphabet[3:5])
+print(alphabet[4:2:-1])
