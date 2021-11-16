0d039b17084e2088ff6e3d347abe268284d82070
kidalo <vladislav.ten1987@gmail.com>
2021-11-16 17:26:40 +0300

    Случайные соревнования

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+import random
+first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+
+result = [first_team[i] if first_team[i] > second_team[i] else second_team[i] for i in range(20)]
+
+print('Первая команда:', first_team)
+print('\nВторая команда:', second_team)
+print('\nПобедители тура:', result)
