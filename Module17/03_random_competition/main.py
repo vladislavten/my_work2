0cac25435e6858d0611100ab60fdb8506a6fabe8
kidalo <vladislav.ten1987@gmail.com>
2021-11-18 13:09:43 +0500

    IP-адрес 2

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,10 +1,22 @@
 # TODO здесь писать код
+# import random
+# first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+# second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+#
+# result = [first_team[i] if first_team[i] > second_team[i] else second_team[i] for i in range(20)]
+#
+# print('Первая команда:', first_team)
+# print('\nВторая команда:', second_team)
+# print('\nПобедители тура:', result)
+
 import random
-first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
-second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
 
-result = [first_team[i] if first_team[i] > second_team[i] else second_team[i] for i in range(20)]
+s1 = [round(random.uniform(5,10),2) for x in range(20)]
+s2 = [round(random.uniform(5,10),2) for x in range(20)]
+s3 = [s1 if s1 >= s2 else s2 for x in range(20)]
 
-print('Первая команда:', first_team)
-print('\nВторая команда:', second_team)
-print('\nПобедители тура:', result)
+result = []
+
+print(s1)
+print(s2)
+print(s3)
--- myWork/18 DZ.py
+++ myWork/18 DZ.py
@@ -42,27 +42,72 @@
 
 
 # Задача 5. Пароль
+#
+# while True:
+#     digit_count = 0
+#     word_count = 0
+#     word_up_count =0
+#     password = input('Придумайте пароль: ')
+#     for i_digit in password:
+#         if i_digit.isdigit():
+#             digit_count += 1
+#
+#     for i_word in password:
+#         if i_word.isalpha():
+#             word_count += 1
+#
+#     for i_word_upper in password:
+#         if i_word_upper.isupper():
+#             word_up_count += 1
+#
+#     if len(password) >= 8 and digit_count >= 3 and word_count >= 5 and word_up_count >= 1:
+#         print('Это надёжный пароль!')
+#         break
+#     else:
+#         print('Не надёжный пароль, попробуйте ещё раз')
 
-while True:
-    digit_count = 0
-    word_count = 0
-    word_up_count =0
-    password = input('Придумайте пароль: ')
-    for i_digit in password:
-        if i_digit.isdigit():
-            digit_count += 1
 
-    for i_word in password:
-        if i_word.isalpha():
-            word_count += 1
+# Задача 6. Сжатие
+# s = 'ccvvvrRR'
+#
+# # s = input('Введите строку: ')
+# result = ''
+# count = 1
+#
+# for i in range(len(s) - 1):
+#     if s[i] == s[i + 1]:
+#         count += 1
+#     elif s[i] != s[i + 1] or i == len(s) - 2:
+#         result += s[i] + str(count)
+#         count = 1
+#     print(s[i])
+# if s[-2] != s[-1]:
+#     result += s[-1] + str(count)
+# # elif s[-2] == s[-1]:
+# #     result += s[-1] + '2'
+#
+# print('Закодированная строка:', result)
 
-    for i_word_upper in password:
-        if i_word_upper.isupper():
-            word_up_count += 1
 
-    if len(password) >= 8 and digit_count >= 3 and word_count >= 5 and word_up_count >= 1:
-        print('Это надёжный пароль!')
+
+# Задача 7. IP-адрес 2
+
+ip = input('Введите IP: ').split('.')
+flg = True
+
+for i in ip:
+    if not i.isdigit() and flg == True:
+        print(i, 'не целое число')
+        flg = False
         break
-    else:
-        print('Не надёжный пароль, попробуйте ещё раз')
+    elif int(i) > 255:
+        print(i, 'превышает 255')
+        flg = False
+        break
+    elif len(ip) != 4:
+        print('Адрес - это четыре числа, разделённые точками')
+        flg = False
+        break
+if flg == True:
+    print('Корректный IP адрес')
 
