2c48aa7fc0dadf91f3e87652889f09a357c2b279
ten_v <vladislav.ten1987@gmail.com>
2021-11-22 12:35:37 +0500

    Сжатие

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+s = input('Введите строку: ')
+count = 0
+result = ''
+
+for i in range(len(s)):
+    count += 1
+    if i == len(s) - 1:
+        result = result + s[i] + str(count)
+        break
+    if s[i] != s[i+1]:
+        result = result + s[i] + str(count)
+        count = 0
+
+print('Сжатый вариант: ', result)
