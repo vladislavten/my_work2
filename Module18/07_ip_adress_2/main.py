4909d158cecc35909d7315bd6d7cf91f30294b9e
ten_v <vladislav.ten1987@gmail.com>
2021-11-22 12:36:26 +0500

    адрес 2

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+ip = input('Введите IP: ').split('.')
+flg = True
+
+for i in ip:
+    if not i.isdigit() and flg:
+        print(i, 'не целое число')
+        flg = False
+        break
+    elif int(i) > 255:
+        print(i, 'превышает 255')
+        flg = False
+        break
+    elif len(ip) != 4:
+        print('Адрес - это четыре числа, разделённые точками')
+        flg = False
+        break
+
+if flg == True:
+    print('Корректный IP адрес')
