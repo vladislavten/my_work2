e1d5d93a14a5578c299ce0f5330075a0aae75a1d
kidalo <vladislav.ten1987@gmail.com>
2021-11-22 22:41:54 +0500

    Задача 9. Сообщение

--- myWork/19.py
+++ myWork/19.py
@@ -70,21 +70,22 @@
 
 # Задача 2. Кризис фруктов
 
-incomes = {'apple': 5600.20, 'orange': 3500.45, 'banana': 5000.00, 'bergamot': 3700.56, 'durian': 5987.23, 'grapefruit': 300.40, 'peach': 10000.50, 'pear': 1020.00, 'persimmon': 310.00}
+# incomes = {'apple': 5600.20, 'orange': 3500.45, 'banana': 5000.00, 'bergamot': 3700.56, 'durian': 5987.23, 'grapefruit': 300.40, 'peach': 10000.50, 'pear': 1020.00, 'persimmon': 310.00}
+#
+# print('\nОбщий доход за год составил', sum(incomes.values()))
+# print(incomes)
+#
+# name = ''
+# val = max(incomes.values())
+#
+# for i in incomes:
+#     if incomes[i] < val:
+#         val = incomes[i]
+#         name = i
+#
+# print('\nСамый маленький доход у {}. Он составляет {} рублей'.format(name, val))
+#
+# incomes.pop(name)
+# print('\nИтоговый словарь: ', incomes)
 
-print('Общий доход за год составил', sum(incomes.values()))
-print(incomes)
-
-name = ''
-val = max(incomes.values())
-
-for i in incomes:
-    if incomes[i] < val:
-        val = incomes[i]
-        name = i
-
-print('Самый маленький доход у {}. Он составляет {} рублей'.format(name,val))
-
-# minimum = min(incomes.values())
-# print(minimum)
 
