828b1847646cdb1aa2f6c5ba8cfa1c41c3fb5900
ten_v <vladislav.ten1987@gmail.com>
2021-11-22 14:11:09 +0500

    Контакты

---
+++ myWork/19.py
@@ -0,0 +1,55 @@
+
+# Задача 1. Словарь квадратов чисел
+#
+# num = int(input('Число: '))
+# DC = dict()
+#
+#
+# for i in range(1, num + 1):
+#     DC[i] = i ** 2
+#
+# print(DC)
+
+
+
+# Задача 2. Студент
+
+# info = input('Введите информацию о студенте через пробел (имя, фамилия, город, место учёбы, оценки): ').split()
+#
+# dct = dict()
+#
+# dct['Имя'] = info[0]
+# dct['Фамилия'] = info[1]
+# dct['Город'] = info[2]
+# dct['Место учёбы'] = info[3]
+# dct['Оценки'] = []
+#
+# for i in info[4:]:
+#     dct['Оценки'].append(i)
+#
+# for i in dct:
+#     print(i, '-', dct[i])
+
+
+
+# Задача 3. Контакты
+# name = ''
+# phonebook = dict()
+# while name != 'стоп':
+#     print('\nТекущие контакты на телефоне:')
+#
+#     for i in phonebook:
+#         print(i, phonebook[i])
+#
+#     name = input('Введите имя: ')
+#     if name in phonebook:
+#         print('\nТакое имя уже существует')
+#     else:
+#         number = input('Введите номер: ')
+#         phonebook[name] = number
+
+
+
+
+
+
