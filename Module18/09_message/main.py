917761fa832bda1a41e303881035e6ea47429243
ten_v <vladislav.ten1987@gmail.com>
2021-11-22 12:45:52 +0500

    Сообщение

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+message = input('Введите сообщение: ')
+word = ''
+result = ''
+for i in message:
+    if i.isalpha():
+        word += i
+    elif not i.isalpha():
+        word = word[::-1]
+        result += word
+        result += i
+        word = ''
+if len(word) >= 1:
+    print('Результат:', word[::-1])
+else:
+    print('Результат:', result)
