37b11541d80d46253e9dc1095c83e7b9ac20e983
ten_v <vladislav.ten1987@gmail.com>
2021-11-24 17:52:11 +0500

    Криптовалюта

--- myWork/19 DZ.py
+++ myWork/19 DZ.py
@@ -26,24 +26,127 @@
 
 
 # Задача 2. География
+
+#
 # 1 страна: Россия Москва Петербург Новгород
 #
 # 2 страна: Германия Берлин Лейпциг Мюнхен
-nums_country = int(input('Количество стран: '))
-dct = dict()
+#
+# dct = {}
+# nums_country = int(input('Кол-во стран: '))
+# for i in range(0, nums_country):
+#     value = input(f'{i + 1} страна: ').split()
+#     for town in value[1:]:
+#         dct[town] = value[0]
+#
+# for i in range(1, 4):
+#     city = input(f'{i} город: ')
+#     country = dct.get(city)
+#     if country:
+#         print(f'Город {city} расположен в стране {country}.')
+#     else:
+#         print(f'По городу {city} данных нет.')
 
-for i in range(1, nums_country + 1):
-    country = input('{} страна: '.format(i)).split()
-    dct[country[0]] = {country[1], country[2], country[3]}
 
-for number in range(1, 4):
-    city = input('\n{} город: '.format(number))
-    for i in dct:
-        if city in dct[i]:
-            print('Город {} расположен в стране {}.'.format(city, i))
-            break
-        else:
-            print('По городу {} данных нет.'.format(city))
-            break
 
+# Задача 3. Криптовалюта
+data = {
+
+    "address": "0x544444444444",
+
+    "ETH": {
+        "balance": 444,
+        "totalIn": 444,
+        "totalOut": 4
+            },
+
+    "count_txs": 2,
+
+    "tokens": [
+
+        {
+
+            "fst_token_info": {
+
+                "address": "0x44444",
+
+                "name": "fdf",
+
+                "decimals": 0,
+
+                "symbol": "dsfdsf",
+
+                "total_supply": "3228562189",
+
+                "owner": "0x44444",
+
+                "last_updated": 1519022607901,
+
+                "issuances_count": 0,
+
+                "holders_count": 137528,
+
+                "price": False
+
+            },
+
+            "balance": 5000,
+
+            "totalIn": 0,
+
+            "total_out": 0
+
+        },
+
+        {
+
+            "sec_token_info": {
+
+                "address": "0x44444",
+
+                "name": "ggg",
+
+                "decimals": "2",
+
+                "symbol": "fff",
+
+                "total_supply": "250000000000",
+
+                "owner": "0x44444",
+
+                "last_updated": 1520452201,
+
+                "issuances_count": 0,
+
+                "holders_count": 20707,
+
+                "price": False
+
+            },
+
+            "balance": 500,
+
+            "totalIn": 0,
+
+            "total_out": 0
+
+        }
+
+    ]
+
+}
+
+for i in data:
+    print(i, '-', data[i])
+print()
+
+data['ETH'].update({'total_diff' : 100})
+data['tokens'][0]['fst_token_info'].update({'name' : 'doge'})
+data['ETH']['total_out'] = data['tokens'][0].pop('total_out')
+data['tokens'][1]['sec_token_info']['total_price'] = data['tokens'][1]['sec_token_info'].pop('price')
+
+print('Результат манипуляций')
+for i in data:
+    print(i, '-', data[i])
+print()
 
