0eef86d880e81e59f2c970c6b4c0536d01d9b680
ten_v <vladislav.ten1987@gmail.com>
2021-11-25 14:41:36 +0500

    Гистограмма частоты 2

--- myWork/19 DZ.py
+++ myWork/19 DZ.py
@@ -16,7 +16,7 @@
 #
 # for i in range(num):
 #     song = input('Название {} песни: '.format(i + 1))
-#     if song in violator_songs:
+#     if violator_songs.get(song):
 #         all_time += violator_songs[song]
 #     else:
 #         print('Такой песни нет')
@@ -50,103 +50,193 @@
 
 
 # Задача 3. Криптовалюта
-data = {
+# data = {
+#
+#     "address": "0x544444444444",
+#
+#     "ETH": {
+#         "balance": 444,
+#         "totalIn": 444,
+#         "totalOut": 4
+#             },
+#
+#     "count_txs": 2,
+#
+#     "tokens": [
+#
+#         {
+#
+#             "fst_token_info": {
+#
+#                 "address": "0x44444",
+#
+#                 "name": "fdf",
+#
+#                 "decimals": 0,
+#
+#                 "symbol": "dsfdsf",
+#
+#                 "total_supply": "3228562189",
+#
+#                 "owner": "0x44444",
+#
+#                 "last_updated": 1519022607901,
+#
+#                 "issuances_count": 0,
+#
+#                 "holders_count": 137528,
+#
+#                 "price": False
+#
+#             },
+#
+#             "balance": 5000,
+#
+#             "totalIn": 0,
+#
+#             "total_out": 0
+#
+#         },
+#
+#         {
+#
+#             "sec_token_info": {
+#
+#                 "address": "0x44444",
+#
+#                 "name": "ggg",
+#
+#                 "decimals": "2",
+#
+#                 "symbol": "fff",
+#
+#                 "total_supply": "250000000000",
+#
+#                 "owner": "0x44444",
+#
+#                 "last_updated": 1520452201,
+#
+#                 "issuances_count": 0,
+#
+#                 "holders_count": 20707,
+#
+#                 "price": False
+#
+#             },
+#
+#             "balance": 500,
+#
+#             "totalIn": 0,
+#
+#             "total_out": 0
+#
+#         }
+#
+#     ]
+#
+# }
+#
+# for i in data:
+#     print(i, '-', data[i])
+# print()
+#
+# data['ETH'].update({'total_diff' : 100})
+# data['tokens'][0]['fst_token_info'].update({'name' : 'doge'})
+# data['ETH']['total_out'] = data['tokens'][0].pop('total_out')
+# data['tokens'][1]['sec_token_info']['total_price'] = data['tokens'][1]['sec_token_info'].pop('price')
+#
+# print('Результат манипуляций')
+# for i in data:
+#     print(i, '-', data[i])
+# print()
 
-    "address": "0x544444444444",
 
-    "ETH": {
-        "balance": 444,
-        "totalIn": 444,
-        "totalOut": 4
-            },
 
-    "count_txs": 2,
+# Задача 4. Товары
 
-    "tokens": [
+# goods = {
+#
+#     'Лампа': '12345',
+#
+#     'Стол': '23456',
+#
+#     'Диван': '34567',
+#
+#     'Стул': '45678',
+#
+# }
+#
+# store = {
+#
+#     '12345': [
+#
+#         {'quantity': 27, 'price': 42},
+#
+#     ],
+#
+#     '23456': [
+#
+#         {'quantity': 22, 'price': 510},
+#
+#         {'quantity': 32, 'price': 520},
+#
+#     ],
+#
+#     '34567': [
+#
+#         {'quantity': 2, 'price': 1200},
+#
+#         {'quantity': 1, 'price': 1150},
+#
+#     ],
+#
+#     '45678': [
+#
+#         {'quantity': 50, 'price': 100},
+#
+#         {'quantity': 12, 'price': 95},
+#
+#         {'quantity': 43, 'price': 97},
+#
+#     ],
+#
+# }
+#
+# for i in goods:
+#     summ = 0
+#     quantity = 0
+#     if goods[i] in store:
+#         for y in range(len(store[goods[i]])):
+#             summ += store[goods[i]][y]['quantity'] * store[goods[i]][y]['price']
+#             quantity += store[goods[i]][y]['quantity']
+#         print('{} - {} шт, стоимость {} руб'.format(i, quantity, summ))
 
-        {
 
-            "fst_token_info": {
 
-                "address": "0x44444",
+# Задача 5. Гистограмма частоты 2
 
-                "name": "fdf",
+# text = input('Введите текст: ')
+# dct = dict()
+# dct_invert = dict()
+# for i in text:
+#     if i in dct:
+#         dct[i] += 1
+#     else:
+#         dct[i] = 1
+#
+# for i in sorted(dct):
+#     print(i, ':', dct[i])
+#
+#
+# for i in dct:
+#     if dct[i] in dct_invert:
+#         dct_invert[dct[i]].append(i)
+#     else:
+#         dct_invert[dct[i]] = [i]
+#
+# print('\nИнвертированный словарь частот:')
+#
+# for i in dct_invert:
+#     print(i, ':', dct_invert[i])
 
-                "decimals": 0,
-
-                "symbol": "dsfdsf",
-
-                "total_supply": "3228562189",
-
-                "owner": "0x44444",
-
-                "last_updated": 1519022607901,
-
-                "issuances_count": 0,
-
-                "holders_count": 137528,
-
-                "price": False
-
-            },
-
-            "balance": 5000,
-
-            "totalIn": 0,
-
-            "total_out": 0
-
-        },
-
-        {
-
-            "sec_token_info": {
-
-                "address": "0x44444",
-
-                "name": "ggg",
-
-                "decimals": "2",
-
-                "symbol": "fff",
-
-                "total_supply": "250000000000",
-
-                "owner": "0x44444",
-
-                "last_updated": 1520452201,
-
-                "issuances_count": 0,
-
-                "holders_count": 20707,
-
-                "price": False
-
-            },
-
-            "balance": 500,
-
-            "totalIn": 0,
-
-            "total_out": 0
-
-        }
-
-    ]
-
-}
-
-for i in data:
-    print(i, '-', data[i])
-print()
-
-data['ETH'].update({'total_diff' : 100})
-data['tokens'][0]['fst_token_info'].update({'name' : 'doge'})
-data['ETH']['total_out'] = data['tokens'][0].pop('total_out')
-data['tokens'][1]['sec_token_info']['total_price'] = data['tokens'][1]['sec_token_info'].pop('price')
-
-print('Результат манипуляций')
-for i in data:
-    print(i, '-', data[i])
-print()
 
