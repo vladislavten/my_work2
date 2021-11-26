6cd3240fc99cd8a1f8d14f66b796d17bab625e6f
ten_v <vladislav.ten1987@gmail.com>
2021-11-26 17:35:14 +0500

    Пицца

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+n = int(input('Введите кол-во заказов: '))
+d = {}
+for i in range(1, n + 1):
+    order = input(f'{i} заказ: ').split()
+    fio, pizza, amount = order[0], order[1], int(order[2])
+
+    if fio not in d:
+        d[fio] = {pizza: amount}
+    else:
+        if pizza not in d[fio]:
+            d[fio] |= {pizza: amount}
+        else:
+            d[fio][pizza] += amount
+for fio, order in sorted(d.items()):
+    print(f'{fio}:')
+    for pizza, amount in sorted(order.items()):
+        print('\t', pizza, amount)
