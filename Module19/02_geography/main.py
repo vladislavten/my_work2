f7989768bfd6b75587ce92326185d42ce4194800
ten_v <vladislav.ten1987@gmail.com>
2021-11-26 17:29:14 +0500

    География

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+dct = {}
+nums_country = int(input('Кол-во стран: '))
+for i in range(0, nums_country):
+    value = input(f'{i + 1} страна: ').split()
+    for town in value[1:]:
+        dct[town] = value[0]
+print(dct)
+
+for i in range(1, 4):
+    city = input(f'{i} город: ')
+    country = dct.get(city)
+    if country:
+        print(f'Город {city} расположен в стране {country}.')
+    else:
+        print(f'По городу {city} данных нет.')
