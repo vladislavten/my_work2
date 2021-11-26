d8f601a365414434a2d3c9332ef5367e1aac48eb
ten_v <vladislav.ten1987@gmail.com>
2021-11-26 17:36:16 +0500

    Угадай число

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,23 @@
 # TODO здесь писать код
+
+number = {str(i) for i in range(1, int(input('Введите максимальное число: ')) + 1)}
+while True:
+    choice = input('Нужное число есть среди вот этих чисел: ')
+    if choice == 'Помогите':
+        print('Артём мог загадать следующие числа:', sorted(number))
+        break
+    choice = choice.split(' ')
+    choice = set(choice)
+    answer = input('Ответ Артёма: ')
+    if answer == 'Да' or answer == 'да':
+        number = number & choice
+    elif answer == 'нет' or answer == 'Нет':
+        number = number - choice
+
+Задача 9. Родословная
+
+def height(man):
+    if man not in p_tree:
+        return 0
+    else:
+        return 1 + height(p_tree[man])
