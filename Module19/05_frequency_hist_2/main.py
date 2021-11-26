f5b23168f1a24259916949317d5d7253a4e5201c
ten_v <vladislav.ten1987@gmail.com>
2021-11-26 17:33:40 +0500

    Гистограмма частоты 2

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,25 @@
 # TODO здесь писать код
+
+text = input('Введите текст: ')
+dct = dict()
+dct_invert = dict()
+for i in text:
+    if i in dct:
+        dct[i] += 1
+    else:
+        dct[i] = 1
+
+for i in sorted(dct):
+    print(i, ':', dct[i])
+
+
+for i in dct:
+    if dct[i] in dct_invert:
+        dct_invert[dct[i]].append(i)
+    else:
+        dct_invert[dct[i]] = [i]
+
+print('\nИнвертированный словарь частот:')
+
+for i in dct_invert:
+    print(i, ':', dct_invert[i])
