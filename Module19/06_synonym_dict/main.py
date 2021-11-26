9e1cc8e8fbb8befe1b91a5f418cfbb01f0716bdc
ten_v <vladislav.ten1987@gmail.com>
2021-11-26 17:34:31 +0500

    Словарь синонимов

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+dct = dict()
+
+for i in range(1, (int(input('Введите количество пар слов: '))) + 1):
+    pair = input('{} пара: '.format(i)).split(' - ')
+    dct[pair[0]] = pair[1]
+    dct[pair[1]] = pair[0]
+
+while True:
+    word = input('Введите слово: ')
+    if dct.get(word):
+        print('Синоним: ', dct[word])
+    else:
+        print('Такого слова в словаре нет.')
