6d057415a59e23b110fd603051620a33ea1aea43
ten_v <vladislav.ten1987@gmail.com>
2021-11-30 09:26:39 +0500

    Задача 2. Универсальная программа 2

--- myWork/test.py
+++ myWork/test.py
@@ -1,24 +1,47 @@
-def checking_array(checking_list):
-    return [i for i, v in enumerate(checking_list) if checking_num(i)]
+# def checking_array(checking_list):
+#     return [i for i, v in enumerate(checking_list) if checking_num(i)]
+#
+# def checking_num(i_num):
+#     k = 0
+#     for i in range(2, i_num // 2 + 1):
+#         if i_num % i == 0:
+#             k = k + 1
+#         if k <= 0:
+#             return True
+#         else:
+#             return False
+#
+# # s = 'О Дивный Новый мир!'
+# # text = 'О Дивный Новый мир!'
+# # s = ('й', 'р', 'с', 'г', 'а', 'а', 'т', 'ж', 'е', 'к')
+# s = [100, 200, 300, 'буква', 0, 2, 'а']
+# # s = {0: 'д', 1: 'а', 2: 'а', 3: 'в', 4: 'т', 5: 'ж', 6: 'р', 7: 'б', 8: 'й', 9: 'р'}
+#
+# i_list = checking_array(s)
+# print(i_list)
+#
+# for i in i_list:
+#     print(s[i], end = ' ')
 
-def checking_num(i_num):
-    k = 0
-    for i in range(2, i_num // 2 + 1):
-        if i_num % i == 0:
+
+# Листинг 1
+# вводим N
+n = int(input("n="))
+# создаем пустой список для хранения простых чисел
+lst = []
+# в k будем хранить количество делителей
+k = 0
+# пробегаем все числа от 2 до N
+for i in range(2, n+1):
+    # пробегаем все числа от 2 до текущего
+    for j in range(2, i):
+        # ищем количество делителей
+        if i % j == 0:
             k = k + 1
-        if k <= 0:
-            return True
-        else:
-            return False
-
-# s = 'О Дивный Новый мир!'
-# text = 'О Дивный Новый мир!'
-# s = ('й', 'р', 'с', 'г', 'а', 'а', 'т', 'ж', 'е', 'к')
-s = [100, 200, 300, 'буква', 0, 2, 'а']
-# s = {0: 'д', 1: 'а', 2: 'а', 3: 'в', 4: 'т', 5: 'ж', 6: 'р', 7: 'б', 8: 'й', 9: 'р'}
-
-i_list = checking_array(s)
-print(i_list)
-
-for i in i_list:
-    print(s[i], end = ' ')
+    # если делителей нет, добавляем число в список
+    if k == 0:
+        lst.append(i)
+    else:
+        k = 0
+# выводим на экран список
+print (lst)
