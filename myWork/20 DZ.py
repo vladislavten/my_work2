96efd3e531a49bb9b4b7aaea4219b61b72c8874c
kidalo <vladislav.ten1987@gmail.com>
2021-12-01 21:53:09 +0500

    Протокол соревнований

--- myWork/20 DZ.py
+++ myWork/20 DZ.py
@@ -209,22 +209,14 @@
 
 
 # Задача 9. Протокол соревнований
-# dct = dict()
-# for i in range(1, int(input('Сколько записей вносится в протокол? ')) + 1):
-#     enter_result = input(f'{i} запись: ').split()
-#     if not enter_result[1] in dct:
-#         dct[enter_result[1]] = int(enter_result[0]), i
-#     else:
-#         if int(enter_result[0]) > dct[enter_result[1]][0]:
-#             dct[enter_result[1]] = int(enter_result[0]), i
-#
-# print(dct)
-
-# dct = (('Jack', 95715),
-#        ('qwerty', 197128),
-#        ('Alex', 95715),
-#        ('M', 95715)
-#        )
+dct = dict()
+for i in range(1, int(input('Сколько записей вносится в протокол? ')) + 1):
+    enter_result = input(f'{i} запись: ').split()
+    if not enter_result[1] in dct:
+        dct[i] = int(enter_result[0]), enter_result[1]
+    else:
+        if int(enter_result[0]) > dct[enter_result[1]][0]:
+            dct[i] = int(enter_result[0]), enter_result[1]
 
 # dct = {1: (69485, 'Jack'),
 #        2: (95715, 'qwerty'),
@@ -237,40 +229,15 @@
 #        9: (95715, 'M')
 #        }
 
-# for place in range(1, 3 + 1):
-#     a = [0, 0]
-#     for i, v in enumerate(dct):
-#         if dct[v][0] > a[0]:
-#             a = [dct[v][0], v]
-#     print(f'{place} место:', dct[a[1]][1], a[0])
-#     dct.pop(a[1])
-# print(a)
-
-
-
-
-dct = {1: (69485, 'Jack'),
-       2: (95715, 'qwerty'),
-       3: (95715, 'Alex'),
-       4: (83647, 'M'),
-       5: (197128, 'qwerty'),
-       6: (95715, 'Jack'),
-       7: (93289, 'Alex'),
-       8: (95715, 'Alex'),
-       9: (95715, 'M')
-       }
-dct2 = dct.copy()
-
-
 for place in range(1, 3 + 1):
     a = [0, 0]
-    for i, v in enumerate(dct):
+    for v in dct:
         if dct[v][0] > a[0]:
             a = [dct[v][0], v]
     print(f'{place} место:', dct[a[1]][1], a[0])
 
     for_delete = []
-    for index, value in enumerate(dct):
+    for value in dct:
         if dct[value][1] == dct[a[1]][1]:
             for_delete.append(value)
     for delete in for_delete:
