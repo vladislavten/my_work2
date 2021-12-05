faf8550a09bc28b7f5d0f31dc3ddd5412e442dbf
kidalo <vladislav.ten1987@gmail.com>
2021-12-05 23:24:11 +0500

    Задача 3. Поиск элемента

--- myWork/test.py
+++ myWork/test.py
@@ -26,22 +26,61 @@
 
 # Листинг 1
 # вводим N
-n = int(input("n="))
-# создаем пустой список для хранения простых чисел
-lst = []
-# в k будем хранить количество делителей
-k = 0
-# пробегаем все числа от 2 до N
-for i in range(2, n+1):
-    # пробегаем все числа от 2 до текущего
-    for j in range(2, i):
-        # ищем количество делителей
-        if i % j == 0:
-            k = k + 1
-    # если делителей нет, добавляем число в список
-    if k == 0:
-        lst.append(i)
-    else:
-        k = 0
-# выводим на экран список
-print (lst)
+# n = int(input("n="))
+# # создаем пустой список для хранения простых чисел
+# lst = []
+# # в k будем хранить количество делителей
+# k = 0
+# # пробегаем все числа от 2 до N
+# for i in range(2, n+1):
+#     # пробегаем все числа от 2 до текущего
+#     for j in range(2, i):
+#         # ищем количество делителей
+#         if i % j == 0:
+#             k = k + 1
+#     # если делителей нет, добавляем число в список
+#     if k == 0:
+#         lst.append(i)
+#     else:
+#         k = 0
+# # выводим на экран список
+# print (lst)
+
+# def test(a):
+#     if a == 101:
+#         return 1
+#     print(a)
+#     a += 1
+#     test(a)
+#
+# a = 1
+# test(a)
+
+# def sort(nums):
+#     for i in range(len(nums)):
+#          for x in range(i, len(nums)):
+#                if nums[x] < nums[i]:
+#                      nums[x], nums[i] = nums[i], nums[x]
+
+# def foo(start):
+#     if start == 0:
+#         return
+#     print(foo(start - 1))
+#     return start
+#
+#
+# foo(100)
+# print()
+# print()
+
+# простая рекурсия
+
+def foo1(start):
+    if start == 0:
+        return
+
+    print(start)
+    (foo1(start - 1))
+
+
+foo1(100)
