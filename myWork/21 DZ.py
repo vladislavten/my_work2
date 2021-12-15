905effd079a424386583a2b4a00a3304b24c7ca6
ten_v <vladislav.ten1987@gmail.com>
2021-12-15 18:00:35 +0500

    # Задача 3. Помощь другу

---
+++ myWork/21 DZ.py
@@ -0,0 +1,96 @@
+# Задача 1. Challenge 2
+
+# def count(start):
+#     if start == 1:
+#         return start
+#     print(count(start - 1))
+#     return start
+#
+# num = int(input('Введите число: '))
+# count(num + 1)
+#
+
+# Задача 2. Свой zip 2
+
+
+
+# Задача 3. Ряд Фибоначчи
+#
+# def fibonacci(n):
+#     if n in (1, 2):
+#         return 1
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+#
+# n = int(input("Номер элемента ряда Фибоначчи: "))
+# print('Значение этого элемента:', fibonacci(n))
+
+
+# Задача 4. Поиск элемента 2
+
+# site = {
+#     'html': {
+#         'head': {
+#             'title': 'Мой сайт'
+#         },
+#         'body': {
+#             'h2': 'Здесь будет мой заголовок',
+#             'div': 'Тут, наверное, какой-то блок',
+#             'p': 'А вот здесь новый абзац'
+#         }
+#     }
+# }
+#
+# def find_key(struct, key):
+#     if key in struct:
+#         return struct[key]
+#     for i in struct.values():
+#         if isinstance(i, dict):
+#             result = find_key(i, key)
+#             if result:
+#                 break
+#     else:
+#         result = None
+#
+#     return result
+#
+# find = input('Какой ключ ищем? ')
+# result = find_key(site, find)
+# if result:
+#     print(result)
+# else:
+#     print('Такого ключа в структуре сайта нет')
+
+
+
+# Задача 6. Глубокое копирование   (ДОДЕЛАТЬ вывод "Сайт для ...")
+
+# def func(num, name, site):
+#     if num == 0:
+#         return site
+#
+#     site['html']['head']['title'] = f'Куплю/продам телефон {name} недорого'
+#     site['html']['body']['h2'] = f'У нас самая низкая цена на {name}'
+#
+#     print(func(num - 1, name, site))
+#     print(f'Сайт для {name}')
+#     return site
+#
+#
+# site = {
+#     'html': {
+#         'head': {
+#             'title': 'Куплю/продам телефон недорого'
+#         },
+#         'body': {
+#             'h2': 'У нас самая низкая цена на iphone',
+#             'div': 'Купить',
+#             'p': 'продать'
+#         }
+#     }
+# }
+#
+# site_num = int(input('Сколько сайтов: '))
+# name = input('Введите название продукта для нового сайта: ')
+#
+# func(site_num, name, site)
+
