0d02ab4bf534cc2f63ed5c240c0742670428214f
ten_v <vladislav.ten1987@gmail.com>
2021-12-21 14:06:04 +0500

    # Задача 3. Помощь другу

--- myWork/21 DZ.py
+++ myWork/21 DZ.py
@@ -122,6 +122,49 @@
 #
 # func(site_num, site)
 
+############################################################################
+#
+# import copy
+#
+# site = {
+#     'html': {
+#         'head': {
+#             'title': 'Куплю/продам телефон недорого'
+#         },
+#         'body': {
+#             'h2': 'У нас самая низкая цена на iphone',
+#             'div': 'Купить',
+#             'p': 'продать'
+#         }
+#     }
+# }
+#
+#
+# def find_key(struct, key, meaning):
+#     if key in struct:
+#         struct[key] = meaning
+#         return site
+#
+#     for sub_struct in struct.values():
+#         if isinstance(sub_struct, dict):
+#             result = find_key(sub_struct, key, meaning)
+#             if result:
+#                 return site
+#
+#
+#
+# number_sites = int(input('Сколько сайтов: '))
+# site_copy = copy.deepcopy(site)
+# for _ in range(number_sites):
+#     product_name = input('Введите название продукта для нового сайта: ')
+#     key = {'title': f'Куплю/продам {product_name} недорого', 'h2': f'У нас самая низкая цена на {product_name}'}
+#     for i in key:
+#         find_key(site_copy, i, key[i])
+#
+#     print(f'Сайт для {product_name}:')
+#     print(site_copy, '\n')
+
+
 
 # Задача 7. Продвинутая функция sum (сделана)
 
--- myWork/test.py
+++ myWork/test.py
@@ -122,6 +122,44 @@
 
 
 
+# import copy
+#
+# site = {
+#     'html': {
+#         'head': {
+#             'title': 'Куплю/продам телефон недорого'
+#         },
+#         'body': {
+#             'h2': 'У нас самая низкая цена на iphone',
+#             'div': 'Купить',
+#             'p': 'продать'
+#         }
+#     }
+# }
+#
+#
+# def func_all(site):
+#     for _ in range(question):
+#         product = input('Введите название продукта для нового сайта: ').lower()
+#         print(f'Сайт для {product}:')
+#         site_copy = copy.deepcopy(site)
+#         site_copy['html']['head']['title'] = f'Куплю/продам {product} недорого'
+#         site_copy['html']['body']['h2'] = f'У нас самая низкая цена на {product}'
+#         func(site_copy)
+#
+#
+# def func(s, depth=0):
+#     for i in s:
+#         print(' ' * depth, i)
+#         if isinstance(s[i], dict):
+#             func(s[i], depth + 1)
+#         else:
+#             print(' ' * (depth + 1), ' '.join(s[i]))
+#
+#
+# question = int(input('\nСколько будет сайтов: '))
+# func_all(site)
+
 import copy
 
 site = {
@@ -138,24 +176,26 @@ site = {
 }
 
 
-def func_all(site):
-    for _ in range(question):
-        product = input('Введите название продукта для нового сайта: ').lower()
-        print(f'Сайт для {product}:')
-        site_copy = copy.deepcopy(site)
-        site_copy['html']['head']['title'] = f'Куплю/продам {product} недорого'
-        site_copy['html']['body']['h2'] = f'У нас самая низкая цена на {product}'
-        func(site_copy)
+def find_key(struct, key, meaning):
+    if key in struct:
+        struct[key] = meaning
+        return site
+
+    for sub_struct in struct.values():
+        if isinstance(sub_struct, dict):
+            result = find_key(sub_struct, key, meaning)
+            if result:
+                return site
 
 
-def func(s, depth=0):
-    for i in s:
-        print(' ' * depth, i)
-        if isinstance(s[i], dict):
-            func(s[i], depth + 1)
-        else:
-            print(' ' * (depth + 1), ' '.join(s[i]))
 
+number_sites = int(input('Сколько сайтов: '))
+site_copy = copy.deepcopy(site)
+for _ in range(number_sites):
+    product_name = input('Введите название продукта для нового сайта: ')
+    key = {'title': f'Куплю/продам {product_name} недорого', 'h2': f'У нас самая низкая цена на {product_name}'}
+    for i in key:
+        find_key(site_copy, i, key[i])
 
-question = int(input('\nСколько будет сайтов: '))
-func_all(site)
+    print(f'Сайт для {product_name}:')
+    print(site_copy, '\n')
