7e58d466d3bc521110b9ecd00c352bb18a4bde31
ten_v <vladislav.ten1987@gmail.com>
2021-12-23 20:12:53 +0500

    ДЗ Модуль 21

---
+++ myWork/22.py
@@ -0,0 +1,13 @@
+# Задача 1. Сисадмин
+# import os
+#
+# folder = 'access'
+# file_name = 'admin.bat'
+#
+# abs_path = os.path.abspath(os.path.join(folder, file_name))
+# print(abs_path)
+#
+# rel_path = os.path.join(folder, file_name)
+#
+# print(rel_path)
+
