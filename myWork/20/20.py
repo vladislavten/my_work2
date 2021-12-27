16750e1f0b028d9e33f524cc96b806dbfb92aa9b
ten_v <vladislav.ten1987@gmail.com>
2021-12-27 18:00:14 +0500

    ДЗ Модуль 21

---
+++ myWork/20/20.py
@@ -0,0 +1,214 @@
+# 20.2 Кортежи
+
+# Задача 1. Создание кортежей
+#
+
+# import random
+# a = [random.randint(0, 5) for _ in range(10)]
+# a = tuple(a)
+# print(a)
+# b = [random.randint(-5, 0) for _ in range(10)]
+# b = tuple(b)
+# print(b)
+# c = a + b
+# print(c)
+# print(c.count(0))
+
+
+# Задача 2. Цилиндр
+
+# import math
+# def func(r, h):
+#     S = math.pi * r ** 2
+#     side = 2 * math.pi * r * h
+#     full = side + 2 * S
+#     return round(side, 2), round(full, 2)
+#
+# r = int(input('Введите радиус цилиндра: '))
+# h = int(input('Введите высоту цилиндра: '))
+#
+# cort = func(r, h)
+# print(cort[0])
+# print(cort[1])
+#
+#
+
+# Задача 3. Неправильный код
+#
+# import random
+#
+# def change(nums):
+#
+#     index = random.randint(0, 5)
+#     value = random.randint(100, 1000)
+#     nums = list(nums)
+#     nums[index] = value
+#     nums = tuple(nums)
+#     return nums, value
+#
+# my_nums = 1, 2, 3, 4, 5
+#
+# new_nums, rand_val = change(my_nums)
+# print(new_nums, rand_val)
+#
+# new_nums, rand_val2 = change(new_nums)
+# rand_val2 += rand_val
+# print(new_nums, rand_val2)
+
+
+
+# 20.3 Функция enumerate. Перебор нескольких значений
+
+# Задача 1. Саботаж!
+
+# Строка: so~mec~od~e
+
+# string = input('Строка: ')
+# result = []
+# for index, tilda in enumerate(string):
+#     if tilda == '~':
+#         result += str(index)
+# print('Ответ:', ' '.join(result))
+
+
+# Задача 2. Словари из списков
+
+# import random
+# import string
+#
+# def DIC(lst):
+#     dic = {index: values for index, values in enumerate(lst)}
+#     return dic
+#
+#
+# # alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+# #             'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+#
+# frs = [random.choice(string.ascii_lowercase) for _ in range(10)]
+# sec = [random.choice(string.ascii_lowercase) for _ in range(10)]
+#
+# print('\nПервая строка', frs)
+# print('Вторая строка', sec)
+#
+# frsD = DIC(frs)
+# secD = DIC(sec)
+#
+# print('\nПервый словарь:', frsD)
+# print('Второй словарь:', secD)
+
+
+# Задача 3. Универсальная программа
+
+# text = 'О Дивный Новый мир!'
+# text = ('й', 'р', 'с', 'г', 'а', 'а', 'т', 'ж', 'е', 'к')
+# text = [100, 200, 300, 'буква', 0, 2, 'а']
+# text = {0: 'д', 1: 'а', 2: 'а', 3: 'в', 4: 'т', 5: 'ж', 6: 'р', 7: 'б', 8: 'й', 9: 'р'}
+# result = [text[index] for index, values in enumerate(text) if index % 2 == 0]
+
+# result = [text[index] for index in range(1, len(text), 2)]
+
+
+# result = [text[i] for i in range(0, len(text), 2)]
+# print(result)
+
+# 20.4 Перебор ключей и значений в словаре. Метод items
+
+# Задача 1. Кризис миновал
+
+# incomes = {
+#     'apple': 5600.20,
+#     'orange': 3500.45,
+#     'banana': 5000.00,
+#     'bergamot': 3700.56,
+#     'durian': 5987.23,
+#     'peach': 10000.50,
+#     'pear': 1020.00,
+#     'persimmon': 310.00,
+#             }
+#
+# for name, price in incomes.items():
+#     print(name, '--', price)
+
+
+
+# Задача 2. Сервер
+
+#
+# server_data = {
+#
+#     "server": {
+#
+#         "host": "127.0.0.1",
+#
+#         "port": "10"
+#
+#     },
+#
+#     "configuration": {
+#
+#         "access": "true",
+#
+#         "login": "Ivan",
+#
+#         "password": "qwerty"
+#
+#     }
+#
+# }
+#
+# for name in server_data:
+#     print(name)
+#     for key in server_data[name]:
+#         print('   ', key,':', server_data[name][key])
+
+
+# Задача 3. В одну строчку
+
+# print([values for i_key, values in {0: 0, 1: 100, 2: 144, 3: 20, 4: 19}.items() if i_key % 2 == 0])
+
+
+
+# 20.5 Составные ключи
+
+# Задача 1. Паспортные данные
+
+# data = {
+#
+#     (5000, 123456): ('Иванов', 'Василий'),
+#     (6000, 111111): ('Иванов', 'Петр'),
+#     (7000, 222222): ('Медведев', 'Алексей'),
+#     (8000, 333333): ('Алексеев', 'Георгий'),
+#     (9000, 444444): ('Георгиева', 'Мария')
+#         }
+#
+# number = int(input('Введите номер паспорта: '))
+# seriya = int(input('Введите серию паспорта: '))
+# flg = False
+# for index, value in data.items():
+#     if number in index and seriya in index:
+#         print(f'Данные {number} - {seriya} пренадлежат',' '.join(value))
+#         flg = True
+# if flg == False:
+#     print('Совпадений не найдено')
+
+
+# Задача 2. Контакты 2
+
+# phonebook = dict()
+# while True:
+#     IF = input('\nВведите Имя Фамилию через пробел: ').split()
+#     phonenumber = input('Введите номер телефона: ')
+#     IF = tuple(IF)
+#
+#
+#     if IF in phonebook:
+#         print('Такое имя и фамилия уже есть в телефонной книге')
+#         choice = input('Хотите заменить? Да/Нет  ')
+#         if choice == 'Да' or choice == 'да':
+#             phonebook[IF] = phonenumber
+#     else:
+#         phonebook[IF] = phonenumber
+#
+#
+#     print('\n',phonebook)
+
--- myWork/22.py
+++ myWork/22.py
@@ -14,7 +14,6 @@
 # print(rel_path)
 
 
-
 # Задача 2. Содержимое
 
 # import os
@@ -35,3 +34,77 @@
 
 
 
+
+# 22.2 Модуль os: проверки
+
+# Задача 1. Иконки
+
+# import os
+#
+# file = 'C:\Python_Basic'
+# true_path = os.path.abspath(file)
+# print('Путь:', true_path)
+# result = 0
+# if os.path.isfile(true_path):
+#     print('Это файл')
+#     print('Размер файла:', os.path.getsize(true_path), 'байт')
+# elif os.path.isdir(true_path):
+#     for i_elem in os.listdir(true_path):
+#         print(os.path.join(true_path, i_elem))
+#         result += os.path.getsize(true_path)
+#         print(result)
+#     print('Это папка')
+#     print('Размер папки:', result, 'байт')
+# else:
+#     print('Указанного пути не существует')
+
+
+
+# Задача 2. Поиск файла
+
+# import os
+#
+# def find(path, name):
+#     for i_elem in os.listdir(path):
+#         if os.path.isdir(os.path.join(path, i_elem)):
+#             find(os.path.join(path, i_elem), name)
+#         if os.path.isfile(os.path.join(path, i_elem, name)):
+#             print(os.path.join(path, i_elem, name))
+#
+# abs_path = 'C:\Python_Basic'
+# true_path = os.path.abspath(abs_path)
+# file_name = '111.txt'
+#
+# find(true_path, file_name)
+
+
+import os
+
+# print(os.path.abspath(os.path.join(os.path.sep, 'task', 'group_1.txt' )))
+
+file = open(os.path.abspath(os.path.join(os.path.sep, 'task', 'group_1.txt')), 'r', encoding='utf-8')
+# data = file.read()
+
+summa = 0
+for i_line in file:
+    info = i_line.split()
+    print(info)
+    summa += int(info[2])
+
+# file = open('E:\task\group_1.txt', 'read')
+# diff = 0
+#
+# for i_line in file:
+#     info = i_line.split()
+#     diff -= info[2]
+#
+# file_2 = open('E:\task\group_2.txt', 'read')
+# compose = 0
+#
+# for i_line in file:
+#     info = i_line.split()
+#     compose *= info[2]
+
+print(summa)
+# print(diff)
+# print(compose)
