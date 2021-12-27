88e50903814b10d1ea09856cc8879cadb01e7652
kidalo <vladislav.ten1987@gmail.com>
2021-12-27 23:26:04 +0500

    # Задача 2. Всё в одном

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,24 @@
 # TODO здесь писать код
+
+num = open('numbers.txt', 'r')
+summ = 0
+flg = True
+
+for i_num in num:
+    if i_num == '\n':
+        flg = False
+    else:
+        summ += int(i_num)
+num.close()
+
+summ = str(summ)
+answer = open('answer.txt', 'w')
+answer.write(summ)
+
+answer = open('answer.txt', 'r')
+data = answer.read()
+answer.close()
+
+print('Содержимое файла answer.txt')
+print(data)
+
