f2c7cfda457ceba622f3406f773e1a3972ffa63f
ten_v <vladislav.ten1987@gmail.com>
2021-12-28 18:00:53 +0500

    Задача 2. Дзен Пайтона

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+import os
+
+enter_path = 'C:\Python_Basic'
+
+size = os.path.getsize(enter_path)
+general_size = 0
+count = 0
+for i_elem in os.listdir(enter_path):
+    general_size += os.path.getsize(os.path.join(enter_path, i_elem))
+    count += 1
+
+print(general_size)
+print(count)
