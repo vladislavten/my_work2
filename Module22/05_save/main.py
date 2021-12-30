2beef8c4ac2116f2d14910d0c30037a7495228c8
ten_v <vladislav.ten1987@gmail.com>
2021-12-30 17:59:14 +0500

    Задача 2. Дзен Пайтона

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+import os
+#
+# text = input('Введите строку: ')
+# save_path = input('Куда хотите сохранить документ? Введите последовательность папок (через пробел): ').split(' ')
+# file_name = input('Введите имя файла: ')
+
+text = 'testiruyem'
+save_path = 'Python_Basic Module22'.split(' ')
+file_name = 'my_document'
+format_file = '.txt'
+
+rel_path = os.path.sep.join(save_path)
+
+print(os.path.abspath(os.path.join(os.path.sep, rel_path, file_name + '.txt')))
+
+file = open('os.path.abspath(os.path.join(os.path.sep, rel_path, file_name + format_file', 'w')
+
+print(os.path.abspath(os.path.join(os.path.sep, rel_path, file_name + format_file)))
