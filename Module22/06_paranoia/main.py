05ef8be008bf24b02855b7f0c70029fce1de24d1
ten_v <vladislav.ten1987@gmail.com>
2021-12-31 16:02:12 +0500

    Задача 6. Паранойя

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,11 +1,28 @@
 # TODO здесь писать код
-file = open('text.txt', 'w')
 
-alpha = ' abcdefghijklmnopqrstuvwxyz'
-n = int(input())
-s = input().strip().lower()
-print(s)
-res = ''
-for c in s:
-    res += alpha[(alpha.index(c) + n) % len(alpha)]
-print('Result: "' + res + '"')
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+move = 1
+file = open('text.txt', 'r')
+text_string = file.read().split('\n')
+print('Содержимое файла text.txt:')
+for i_elem in text_string:
+    print(i_elem)
+file.close()
+print()
+print('Содержимое файла cipher_text.txt:')
+for i in text_string:
+    res = ''
+    for x in i:
+        res += alpha[(alpha.index(x.lower()) + move) % len(alpha)]
+    file_write = open('cipher_text.txt', 'a')
+    file_write.write(res + '\n')
+    move += 1
+    file_write.close()
+
+file_write = open('cipher_text.txt', 'r')
+result = file_write.read().split()
+
+for i_elem in result:
+    print(i_elem)
+
