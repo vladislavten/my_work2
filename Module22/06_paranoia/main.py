699835d8aa026009c0579762859947ac83ca666c
ten_v <vladislav.ten1987@gmail.com>
2021-12-31 14:25:44 +0500

    Задача 6. Паранойя

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+file = open('text.txt', 'w')
+
+alpha = ' abcdefghijklmnopqrstuvwxyz'
+n = int(input())
+s = input().strip().lower()
+print(s)
+res = ''
+for c in s:
+    res += alpha[(alpha.index(c) + n) % len(alpha)]
+print('Result: "' + res + '"')
