9a29ba543cb3e88bf83e268de7caf4477ba6bc96
ten_v <vladislav.ten1987@gmail.com>
2021-12-31 17:56:10 +0500

    Задача 7. Турнир

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -1,1 +1,45 @@
 # TODO здесь писать код
+first_tour_file = open('first_tour.txt', 'r')
+
+# for i in first_tour_file:
+#     print(i.split())
+
+# file = first_tour_file.read().split()
+
+flg = True
+data = {}
+count = 0
+ball = 0
+players_id = []
+for i_elem in first_tour_file: # вытаскаиваю проходной бал и заполняю в словарь data игроков из файла first_tour.txt
+    if flg:
+        ball = i_elem
+        flg = False
+    else:
+        data[i_elem.split()[2]] = [i_elem.split()[0], i_elem.split()[1]]
+
+for i_player in data: #Считаю у кого больше 80 баллов
+    if i_player > ball:
+        count += 1
+
+
+#Записываю в файл second_tour.txt у кого больше 80 баллов
+second_tour_file = open('second_tour.txt', 'w')
+second_tour_file.write(str(count) + '\n')
+second_tour_file.close()
+second_tour_file = open('second_tour.txt', 'a')
+
+
+# for index, value in enumerate(data):
+#     if index > count - 1:
+#         second_tour_file.write(max(data) + '\n')
+
+
+
+
+
+
+
+
+print(ball)
+print(data)
