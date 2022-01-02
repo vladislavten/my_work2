bba311843736a9b979a50a95dc96e4a1c81596df
kidalo <vladislav.ten1987@gmail.com>
2022-01-02 12:38:53 +0500

    Задача 8. Частотный анализ

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+str = open("text.txt", "r").read().lower()
+f = {}
+n = 0
+for c in str:
+   if ord('a') <= ord(c) <= ord('z'):
+       x = f.get(c, 0)
+       f[c] = x + 1
+       n += 1
+lout = [(k, "{:5.3f}".format(f[k]/n)) for k in f.keys()]
+lout.sort(key = lambda x: x[0])
+lout.sort(key = lambda x: x[1], reverse = True)
+sout = "\n".join([i[0] + " " + i[1] for i in lout])
+open("analysis.txt","w").write(sout)
