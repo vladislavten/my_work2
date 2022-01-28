099668436ddeb0427299ee3686851a5cef8d2951
ten_v <vladislav.ten1987@gmail.com>
2022-01-28 20:37:06 +0500

    Геттеры Сеттеры

--- myWork/25.py
+++ myWork/25.py
@@ -35,39 +35,43 @@
 
 # Задача 2. Человек
 
-class Person:
-    __count = 0
-
-    def __init__(self, name, age):
-        self.set_age(age)
-        self.set_name(name)
-        Person.__count += 1
-
-    def set_name(self, name):
-        if isinstance(name, str):
-            self.__name = name
-
-    def set_age(self, age):
-        if age in range(0, 100):
-            self.__age = age
-
-    def get_name(self):
-        return self.__name
-
-    def get_age(self):
-        return self.__age
-
-
-man = Person('Вася', 56)
-
-print(man.get_name(), man.get_age())  # Получение данных ГЕТТЕРОМ
-old = 15
-man.set_age(old)  # Замена возраста СЕТТЕРОМ
-print(man.get_name(), man.get_age())
-
-man._Person__age = 10  # Замена возраста у скрытого объекта
-print(man.get_name(), man.get_age())
-
-new_name = 'Вова'
-man.set_name(new_name)  # Замена имени СЕТТЕРОМ
-print(man.get_name(), man.get_age())
+# class Person:
+#     __count = 0
+#
+#     def __init__(self, name, age):
+#         self.set_age(age)
+#         self.set_name(name)
+#         Person.__count += 1
+#
+#     def set_name(self, name):
+#         if isinstance(name, str):
+#             self.__name = name
+#         else:
+#             raise Exception('Ошибка имени')
+#
+#     def set_age(self, age):
+#         if age in range(0, 100):
+#             self.__age = age
+#         else:
+#             raise Exception('Ошибка возраста')
+#
+#     def get_name(self):
+#         return self.__name
+#
+#     def get_age(self):
+#         return self.__age
+#
+#
+# man = Person('Вася', 56)
+#
+# print(man.get_name(), man.get_age())  # Получение данных ГЕТТЕРОМ
+# old = 15
+# man.set_age(old)  # Замена возраста СЕТТЕРОМ
+# print(man.get_name(), man.get_age())
+#
+# man._Person__age = 10  # Замена возраста у скрытого объекта
+# print(man.get_name(), man.get_age())
+#
+# new_name = 'Вова'
+# man.set_name(new_name)  # Замена имени СЕТТЕРОМ
+# print(man.get_name(), man.get_age())
