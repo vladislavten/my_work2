b24bcf8b554029bbefdf3a995c0323a0346d3d6e
kidalo <vladislav.ten1987@gmail.com>
2022-02-01 22:53:04 +0500

    Задача 4. Компания

--- Module25/04_company/main.py
+++ Module25/04_company/main.py
@@ -1,1 +1,27 @@
 # TODO здесь писать код
+class Person:
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+
+class Employee(Person):
+    def __init__(self, name, surname, age):
+        super().__init__(name, surname, age)
+
+    def salary(self):
+        pass
+
+
+class Manager(Employee):
+    def __init__(self, name, surname, age):
+        super().__init__(name, surname, age)
+        self.salary_sum = 13000
+
+    def salary(self):
+        print(self.salary_sum)
+
+
+test = Manager('имя', 'фамилия', 30)
+test.salary()
