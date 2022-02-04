9801f1147b09bf9ecefc48fc08828e345438afa2
ten_v <vladislav.ten1987@gmail.com>
2022-02-04 17:43:11 +0500

    Задача 2. Карма

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,20 +1,20 @@
 import random
 from random import randint, choice
-KARMA = 500
+
+karma = 500
 
 
 def one_day():
-    karma_result = randint(1, 7)
-    if random.choices((0, 1), (1 - 1 / 10, 1 / 10))[0] == 1:
+    if random.randint(1, 10) == 1:
         choose_exception = choice(['KillError', 'DrunkError', 'CarCrashError', 'GluttonyError', 'DepressionError'])
         with open('karma.log', 'a') as log:
             log.write(choose_exception + '\n')
-    return karma_result
+    return randint(1, 7)
 
 
 karma_person = 0
 while True:
-    if karma_person >= KARMA:
+    if karma_person >= karma:
         break
     one_day()
     karma_person += one_day()
