3cce1ba931a97eb38e5de2df4baa231a432249f0
Vladislav Ten <vladislav.ten1987@gmail.com>
2022-02-20 19:11:43 +0000

    Module 27

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import Callable
+
+
+def counter(func: Callable) -> Callable:
+    """
+    Декоратор, который считает количество вызова функции.
+    """
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        return func(*args, **kwargs)
+
+    wrapper.count = 0
+    return wrapper
+
+
+@counter
+def hello():
+    print('Hello world')
+
+
+hello()
+hello()
+hello()
+
+print('Функция запускалась: {} раз(а)'.format(hello.count))
