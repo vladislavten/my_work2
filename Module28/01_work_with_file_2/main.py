a8381d54fc58827a4d936a0a9fbde75a8eb94ea9
Vladislav Ten <vladislav.ten1987@gmail.com>
2022-02-23 22:00:50 +0000

    Module 28

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+import os
+
+
+class File:
+    """
+    Контекст менеджер. При попытке открыть несуществующий файл,
+    менеджер автоматически создаёт и открывает этот файл в
+    режиме записи
+    """
+    def __init__(self, name: str, read_write: str) -> None:
+        self.name = name
+        self.read_write = read_write
+        self.exists_flag = False
+
+    def __enter__(self):
+        if not os.path.exists(self.name):
+            self.file = open(self.name, self.read_write, encoding='utf-8')
+            self.exists_flag = True
+            print('Файл создан. Данные записаны в файл: {}'.format(self.name))
+            return self.file
+        else:
+            print('Файл существует')
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        if self.exists_flag:
+            self.file.close()
+        return True
+
+
+with File('example.txt', 'w') as file:
+    file.write('Всем привет!')
+    test_error = 1 / 0
--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+class Date:
+    """
+    Класс Дата
+    Проверяяет правильность даты и выводит на экран
+    правильно (10-12-2022)
+    НЕ правильно (10, 20, 30)
+    """
+
+    def __init__(self, day: int, month: int, year: int):
+        self.day, self.month, self.year = day, month, year
+
+    def __str__(self) -> str:
+        return f'День: {self.day}   Месяц: {self.month}   Год: {self.year}'
+
+    @classmethod
+    def is_date_valid(cls, date: str) -> bool:
+        day, month, year = map(int, date.split('-'))
+        return 0 < day <= 31 and 0 < month <= 12 and 0 < year <= 9999
+
+    @classmethod
+    def from_string(cls, date: str) -> 'Date':
+        day, month, year = map(int, date.split('-'))
+        date_obj = cls(day, month, year)
+        return date_obj
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
