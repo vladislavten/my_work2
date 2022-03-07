66ce4826b42ab8f7b2a98b57a0bb8cacc6d8de0b
kidalo <vladislav.ten1987@gmail.com>
2022-03-07 23:12:25 +0500

    Задача 4. Читабельность кода

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,16 +1,13 @@
 
+print(list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(x ** 0.5) + 1))), range(2, 100 + 1))))
 
 
-
-print(' '.join(['-' if i % j == 0 else str(i) for i in range(2, 100+1) for j in range(2, i)]))
-
-
-lst = []
+result = []
 for i in range(2, 100+1):
     for j in range(2, i):
         if i % j == 0:
             break
     else:
-        lst.append(str(i))
+        result.append(str(i))
 
-print(' '.join(lst))
+print(' '.join(result))
