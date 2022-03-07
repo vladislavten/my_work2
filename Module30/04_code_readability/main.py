1184f91a421f6b5a69bd6b5ba3d308e8943876d9
kidalo <vladislav.ten1987@gmail.com>
2022-03-07 15:29:14 +0500

    Задача 4. Читабельность кода

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+
+
+
+
+print(' '.join(['-' if i % j == 0 else str(i) for i in range(2, 100+1) for j in range(2, i)]))
+
+
+lst = []
+for i in range(2, 100+1):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        lst.append(str(i))
+
+print(' '.join(lst))
