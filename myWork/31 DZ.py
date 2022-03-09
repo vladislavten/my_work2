321bd9543ff8ae0fed9a499c10eef5f43ea333f6
kidalo <vladislav.ten1987@gmail.com>
2022-03-09 23:33:42 +0500

    31

--- myWork/31 DZ.py
+++ myWork/31 DZ.py
@@ -13,7 +13,6 @@
 # print(result)
 
 
-
 # Задача 2. Регистрационные знаки
 
 
@@ -28,37 +27,41 @@
 
 # Задача 3. Во все тяжкие
 
-import json, requests, re
-
-my_req = requests.get('https://www.breakingbadapi.com/api/deaths') # Список погибших
-general_death_req = requests.get('https://www.breakingbadapi.com/api/death-count') # Парсинг общего числа смертей
-episodes_req = requests.get('https://www.breakingbadapi.com/api/episodes')
-
-data = json.loads(my_req.text)
-deathCount = json.loads(general_death_req.text)
-episodes = json.loads(episodes_req.text)
-
-
-number_of_deaths = max(data, key=lambda elem: elem['number_of_deaths'])
-
-list_of_death = list(map(lambda x: x['death'], data))
-
-episode_id = {}
-for i in episodes:
-       if i['episode'] == '13' and i['season'] == '2':
-              episode_id = i
-              break
-
-
-print('ID эпизода:', episode_id['episode_id'])
-print('Номер сезона:', number_of_deaths['season'])
-print('Номер эпизода:', number_of_deaths['episode'])
-print('Общее число смертей:', deathCount[0]['deathCount']) #Общее число смертей
-print('Список погибших:', ', '.join(list_of_death))
-print('=' * 40)
-
-with open('Breaking_Bad.json', 'w') as file:
-       json.dump(number_of_deaths, file, indent=4)
+# import json, requests
+#
+# my_req = requests.get('https://www.breakingbadapi.com/api/deaths')  # Список погибших
+# general_death_req = requests.get('https://www.breakingbadapi.com/api/death-count')  # Парсинг общего числа смертей
+# episodes_req = requests.get('https://www.breakingbadapi.com/api/episodes')
+#
+# data = json.loads(my_req.text)
+# deathCount = json.loads(general_death_req.text)
+# episodes = json.loads(episodes_req.text)
+#
+# number_of_deaths = max(data, key=lambda elem: elem['number_of_deaths'])
+#
+# list_of_death = list(map(lambda x: x['death'], data))
+#
+# episode_id = {}
+# for i_elem in episodes:
+#     if i_elem['episode'] == '13' and i_elem['season'] == '2':
+#         episode_id = i_elem
+#         break
+#
+# print('ID эпизода:', episode_id['episode_id'])
+# print('Номер сезона:', number_of_deaths['season'])
+# print('Номер эпизода:', number_of_deaths['episode'])
+# print('Общее число смертей:', deathCount[0]['deathCount'])
+# print('Список погибших:', ', '.join(list_of_death))
+#
+# result = {'episode_id': episode_id['episode_id'],
+#           'season': number_of_deaths['season'],
+#           'episode': number_of_deaths['episode'],
+#           'deathCount': deathCount[0]['deathCount'],
+#           'death:': list_of_death
+#           }
+#
+# with open('Breaking_Bad.json', 'w') as file:
+#     json.dump(result, file, indent=4)
 
 # Задача 4. Телефонные номера
 
@@ -72,7 +75,6 @@ with open('Breaking_Bad.json', 'w') as file:
 #               print(count + 1, 'номер: Всё в порядке')
 #        else:
 #               print(count + 1, 'номер: не подходит')
-
 
 
 # Задача 5. Пин-код
@@ -100,33 +102,3 @@ with open('Breaking_Bad.json', 'w') as file:
 # <h3 id="contents>CONTENTS</h3>
 #
 # <h3 id="contents">CONTENTS</h3>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
