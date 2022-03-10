be8f305fc5f8d3d6e6af466367f0f9557ab16931
ten_v <vladislav.ten1987@gmail.com>
2022-03-10 09:45:06 +0500

    Задача 3. Во все тяжкие

--- myWork/31 DZ.py
+++ myWork/31 DZ.py
@@ -28,17 +28,53 @@
 
 # Задача 3. Во все тяжкие
 
+
+# import re
+# numbers_car = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+#
+# private_car = re.findall(r'\b[АВЕКМНОРСТУХ]\d{3}\w{2}\d{2,3}', numbers_car)
+# taxi = re.findall(r'\b[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}',numbers_car)
+# print('Список номеров частных автомобилей:', private_car)
+# print('Список номеров такси:', taxi)
+
+
+# Задача 3. Во все тяжкие
+
 # import json, requests
 #
-# my_req = requests.get('https://www.breakingbadapi.com/api/deaths')
-#
+# my_req = requests.get('https://www.breakingbadapi.com/api/deaths')  # Список погибших
+# general_death_req = requests.get('https://www.breakingbadapi.com/api/death-count')  # Парсинг общего числа смертей
+# episodes_req = requests.get('https://www.breakingbadapi.com/api/episodes')
 #
 # data = json.loads(my_req.text)
-# print(len(data))
-# print(data)
+# deathCount = json.loads(general_death_req.text)
+# episodes = json.loads(episodes_req.text)
+#
+# number_of_deaths = max(data, key=lambda elem: elem['number_of_deaths'])
+#
+# list_of_death = list(map(lambda x: x['death'], data))
+#
+# episode_id = {}
+# for i_elem in episodes:
+#     if i_elem['episode'] == '13' and i_elem['season'] == '2':
+#         episode_id = i_elem
+#         break
+#
+# print('ID эпизода:', episode_id['episode_id'])
+# print('Номер сезона:', number_of_deaths['season'])
+# print('Номер эпизода:', number_of_deaths['episode'])
+# print('Общее число смертей:', deathCount[0]['deathCount'])
+# print('Список погибших:', ', '.join(list_of_death))
+#
+# result = {'episode_id': episode_id['episode_id'],
+#           'season': number_of_deaths['season'],
+#           'episode': number_of_deaths['episode'],
+#           'deathCount': deathCount[0]['deathCount'],
+#           'death:': list_of_death
+#           }
 #
 # with open('Breaking_Bad.json', 'w') as file:
-#        json.dump(data, file, indent=4)
+#     json.dump(result, file, indent=4)
 
 
 
