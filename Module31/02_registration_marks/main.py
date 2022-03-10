24da7356a16234dc8cd9e81ff3618392a7e12e27
ten_v <vladislav.ten1987@gmail.com>
2022-03-10 09:39:58 +0500

    Задача 2. Регистрационные знаки

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+import re
+numbers_car = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+
+private_car = re.findall(r'\b[АВЕКМНОРСТУХ]\d{3}\w{2}\d{2,3}', numbers_car)
+taxi = re.findall(r'\b[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}', numbers_car)
+print('Список номеров частных автомобилей:', private_car)
+print('Список номеров такси:', taxi)
