39f3acac658f2bacd86bfab9662399794743798f
ten_v <vladislav.ten1987@gmail.com>
2022-03-10 09:53:43 +0500

    Задача 6. Web scraping

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import requests, re
+
+my_req = requests.get('http://www.columbia.edu/~fdc/sample.html')
+
+find_data = re.findall(r'(<h3 id=.*>)(.*?)(</h3>)', my_req.text)
+result = []
+for i in find_data:
+    result.append(i[1])
+
+print(result)
