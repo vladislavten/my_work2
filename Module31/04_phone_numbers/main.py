d1d88dbc3dc93b8b26b0c1967c63d64b740dc806
vladislavten <vladislav.ten1987@gmail.com>
2023-01-27 19:23:57 +0500

    Задача 6. Web scraping

--- Module31/04_phone_numbers/main.py
+++
@@ -1,9 +0,0 @@
-import re
-
-phone_numbers = ['9999999999', '999999-999', '99999x9999', '8525654']
-
-for count, item in enumerate(phone_numbers):
-    if re.search(r'\b[8, 9]\d{9}', item):
-        print(count + 1, 'номер: Всё в порядке')
-    else:
-        print(count + 1, 'номер: не подходит')
--- Module31/05_pin_code/main.py
+++
@@ -1,4 +0,0 @@
-import itertools
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-print(list(itertools.combinations(numbers, 4)))
