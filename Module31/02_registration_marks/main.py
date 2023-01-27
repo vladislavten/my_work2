ebb6e5ee5171d35eb814e206e604ca8b1c2f143d
vladislavten <vladislav.ten1987@gmail.com>
2023-01-27 13:59:39 +0500

    Задача 6. Web scraping

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -5,3 +5,4 @@ private_car = re.findall(r'\b[АВЕКМНОРСТУХ]\d{3}\w{2}\d{2,3}', numbe
 taxi = re.findall(r'\b[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}', numbers_car)
 print('Список номеров частных автомобилей:', private_car)
 print('Список номеров такси:', taxi)
+
--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,6 +1,6 @@
 import re
 
-phone_numbers = ['9999999999', '999999-999', '99999x9999']
+phone_numbers = ['9999999999', '999999-999', '99999x9999', '8525654']
 
 for count, item in enumerate(phone_numbers):
     if re.search(r'\b[8, 9]\d{9}', item):
